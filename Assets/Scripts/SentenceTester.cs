72f5a8aea2a82c79f406cd6cee04a2900837609e
Jason Li <jasonli1337@gmail.com>
2020-11-27 15:11:28 -0500

    Sentence Puzzle Object
    Creates SO with blank, data, and myth sentences and can read keys and figure out what it is supposed to be.

---
+++ Assets/Scripts/SentenceTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SentenceTester : MonoBehaviour
+{
+    public SentencePuzzle sp;
+    public List<CardSO> cardSOs;
+
+    public Card c1,c2,c3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp.Clear();
+        sp.Init();
+
+        sp.AddSelection(c1);
+
+        Debug.Log(sp.PartialSentence);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
