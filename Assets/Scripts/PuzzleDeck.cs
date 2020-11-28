bb859947bfbdc62a49fcbbd78a2bdf22017e9bbe
Jason Li <jasonli1337@gmail.com>
2020-11-27 21:38:59 -0500

    Scene And Puzzle Set UP

--- Assets/Scripts/PuzzleDeck.cs
+++ Assets/Scripts/PuzzleDeck.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using EasyButtons;
+using System;
 
 public class PuzzleDeck : MonoBehaviour {
+
+    public Action ResetRing;
     
     //Our Puzzles for this Deck, played in ORDER
     [SerializeField]
@@ -11,7 +14,7 @@ public class PuzzleDeck : MonoBehaviour {
 
     public List<GameObject> cards = new List<GameObject>();
 
-    public DeckRing ring;
+    public float ringRadius;
 
     [Header("Card Prototypes")]
     [SerializeField]
@@ -21,7 +24,11 @@ public class PuzzleDeck : MonoBehaviour {
 
     void Start(){
 
-        //initialize if necessary
+    }
+
+    [Button]
+    public void InitializePuzzle(){
+       //initialize if necessary
         foreach ( SentencePuzzle sp in Puzzles ){
            
             sp.Init(); //initialize the SP
@@ -32,9 +39,7 @@ public class PuzzleDeck : MonoBehaviour {
                 cards.Add(go);
             }
         }
-
     }
-
     GameObject createCard(CardSO properties){
         GameObject go = Instantiate(cardPrefab);
         go.name = properties.name;
@@ -43,9 +48,26 @@ public class PuzzleDeck : MonoBehaviour {
     }
 
     [Button]
-    void SetRing(){
-        ring.DeleteCards();
-        ring.cards = cards;
+    public void PlaceCards(){
+        for (int i = 0 ; i < cards.Count ; i++ ){
+            float a = i * Mathf.PI * 2f / cards.Count;
+            Vector2 offset = new Vector2(ringRadius * Mathf.Cos(a), ringRadius * Mathf.Sin(a));
+            Vector3 pos = new Vector3(transform.position.x + offset.x, transform.position.y + 2f * i / cards.Count, transform.position.z + offset.y);
+            Quaternion rot =  Quaternion.Euler(0f, 90f - 360 * i / cards.Count, 180f);//face up by default
+            //Y axis rotation to match the position on the ring
+            //Z axis rotation to flip face up, an artifact of the prefab
+            cards[i].transform.position = pos;
+            cards[i].transform.localRotation = rot;
+        }
     }
+
+    [Button]
+    public void FlipUpCards(){
+        for (int i = 0 ; i < cards.Count ; i++ ){
+            Quaternion rot =  Quaternion.Euler(0f, 90f - 360 * i / cards.Count, 180f);
+            cards[i].transform.localRotation = rot;
+        }
+    }
+
 
 }
