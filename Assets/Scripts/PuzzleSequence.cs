fb9d3e058355985d71aa2506719e312771b33461
Jason Li <jasonli1337@gmail.com>
2020-11-27 21:50:51 -0500

    Puzzle Sequence init

---
+++ Assets/Scripts/PuzzleSequence.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleSequence : MonoBehaviour
+{
+    [SerializeField]
+    SlideshowHolder Enter, Exit;
+
+    [SerializeField]
+    PuzzleDeck Deck;
+
+    [SerializeField]
+    SentencePuzzle activePuzzle;
+
+    [SerializeField]
+    int puzzleNumber;
+
+    [SerializeField]
+    PuzzleSequence NextPuzzle;
+
+
+}
